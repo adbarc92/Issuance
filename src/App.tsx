1cd1e6d8fa7e7d37843a062a04901bf973d2d437
adbarc92 <adbarclay92@gmail.com>
2020-10-19 10:42:49 -0600

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = (): JSX.Element => {
-  return <div>This is not the next Jira</div>;
+  return (
+    <div>
+      This is not the next Jira<div>Formatting Test</div>
+    </div>
+  );
 };
 
 export default App;
