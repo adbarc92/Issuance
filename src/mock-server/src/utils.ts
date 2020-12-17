f47464c9ad1a9751473a90f3722c805a190d3f91
adbarc92 <adbarclay92@gmail.com>
2020-12-16 19:11:02 -0700

    Scaffolds utilities for SHA256 encryption

--- src/mock-server/src/utils.ts
+++ src/mock-server/src/utils.ts
@@ -1,4 +1,32 @@
+import { randomBytes, createHmac } from 'crypto-js';
+
 // Standardizes error messages for later handling, client-side
 export const createErrorResponse = (errors: string[]): string => {
   return JSON.stringify({ errors });
+};
+
+export const generateRandomString = (length: number): string => {
+  return randomBytes(Math.ceil(length / 2))
+    .toString('hex')
+    .slice(0, length);
+};
+
+interface PasswordData {
+  salt: string;
+  passwordHash: string;
+}
+
+export const sha256 = (password: string, salt: string): PasswordData => {
+  const hash = createHmac('sha256', salt);
+  hash.update(password);
+  const value = hash.digest('hex');
+  return {
+    salt,
+    passwordHash: value,
+  };
+};
+
+export const saltHashPassword = (userpassword: string): PasswordData => {
+  const salt = generateRandomString(16);
+  return sha256(userpassword, salt);
 };
