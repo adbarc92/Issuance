d7478d11a7a13f8e911babc2a25985be5bce8119
adbarc92 <adbarclay92@gmail.com>
2021-01-02 23:32:47 -0700

    Commits new files

---
+++ src/components/PersonnelPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { styled } from '@material-ui/core';
+
+interface PersonnelPageProps {
+  personnelId: number;
+}
+
+const PersonnelPage = (props: PersonnelPageProps): JSX.Element => {
+  return <div></div>;
+};
+
+export default PersonnelPage;
